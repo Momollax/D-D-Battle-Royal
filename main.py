1e3a04e4e6fc0ddf5d4b000c75a8a21005e7e1d1
noé m <noe.moyen@epitech.eu>
2024-02-26 22:21:38 +0100

    done

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from math import cos, sin, pi
 from matplotlib.image import imread
 from matplotlib.patches import Rectangle, RegularPolygon
 import json
+import os
 
 background_image = imread('./picture/map.PNG')
 house_counter = 1
@@ -94,44 +95,97 @@ def draw_house(x, y, image_path, house_number):
     
 def pick_house_tier():
     tiers = ['T1', 'T2', 'T3']
-    probabilites = [0.5, 0.35, 0.25]
+    probabilites = [0.6, 0.25, 0.15]
     tier_choisi = random.choices(tiers, weights=probabilites, k=1)[0]
     return tier_choisi
 
+
 def pick_items(house_tier):
-#"./data/common.txt"
-#"./data/common_link.txt"
-#"./data/uncommon.txt"
-#"./data/uncommon_link.txt"
-#"./data/rare.txt"
-#"./data/rare_link.txt"
-#"./data/very_rare.txt"
-#"./data/very_rare_link.txt"
     objets = []
     if house_tier == 'T1':
-        probabilites_rareté = [0.8, 0.15, 0.04, 0.01]  # Probabilités pour T1
+        probabilites_rareté = [0.6, 0.25, 0.1, 0.04, 0.01]  # Probabilités pour T1
     elif house_tier == 'T2':
-        probabilites_rareté = [0.6, 0.3, 0.07, 0.03]  # Probabilités pour T2
+        probabilites_rareté = [0.4, 0.3, 0.2, 0.07, 0.03]  # Probabilités pour T2
     elif house_tier == 'T3':
-        probabilites_rareté = [0.3, 0.4, 0.2, 0.1]  # Probabilités pour T3
+        probabilites_rareté = [0.2, 0.35, 0.25, 0.15, 0.05]  # Probabilités pour T3
     nombre_objets = random.randint(2, 6)
     for i in range(nombre_objets):
-        rarete_choisie = random.choices(['commun', 'rare', 'tres rare', 'legendaire'], weights=probabilites_rareté, k=1)[0]
-        objets.append({"id": i+1, "rarete": rarete_choisie})
+        rarete_choisie = random.choices(['common', 'uncommon', 'rare', 'very rare', 'legendary'], weights=probabilites_rareté, k=1)[0]
+        
+        with open("./data/objets.json", "r") as f:
+            objets_disponibles = json.load(f)
+        
+        # Filtrer les objets en fonction de la rareté choisie
+        objets_filtres = [objet for objet in objets_disponibles if objet["rarity"] == rarete_choisie]
+        
+        if objets_filtres:
+            # Choisir aléatoirement un seul objet parmi les objets filtrés
+            objet_choisi = random.choice(objets_filtres)
+            objets.append({"id": i+1, "rarete": rarete_choisie, "item": objet_choisi})
+    
     return objets
 
 def generate_house_loot(house_nbr):
-    loot_data = {}
-    for i in range(house_nbr ):
-        i = i + 1
+    for i in range(house_nbr):
         house_tier = pick_house_tier()
-        items = pick_items(house_tier)
-        loot_data[f"Maison_{i}"] = items
+        items = pick_items(house_tier)  # Passer la liste des objets comme argument
+        
+        # Définir les probabilités en fonction du tier de la maison
+        if house_tier == 'T1':
+            enemy_probability = 0.1
+            trap_probability = 0.15
+        elif house_tier == 'T2':
+            enemy_probability = 0.3
+            trap_probability = 0.4
+        elif house_tier == 'T3':
+            enemy_probability = 0.4
+            trap_probability = 0.5
+        
+        # Générer aléatoirement la présence d'ennemis, de pièges ou des deux en fonction des probabilités
+        presence_enemy = random.choices([True, False], weights=[enemy_probability, 1 - enemy_probability], k=1)[0]
+        presence_trap = random.choices([True, False], weights=[trap_probability, 1 - trap_probability], k=1)[0]
+        
+        if presence_enemy and presence_trap:
+            loot_data = f"Maison_{i + 1}: {house_tier}\n"
+            loot_data += "Ennemis présents\n"
+            loot_data += "Pièges présents\n"
+            loot_data += "\n"
+        elif presence_enemy:
+            loot_data = f"Maison_{i + 1}: {house_tier}\n"
+            loot_data += "Ennemis présents\n"
+            loot_data += "\n"
+        elif presence_trap:
+            loot_data = f"Maison_{i + 1}: {house_tier}\n"
+            loot_data += "Pièges présents\n"
+            loot_data += "\n"
+        else:
+            loot_data = f"Maison_{i + 1}: {house_tier}\n"
+            loot_data += "Aucun ennemi ni piège\n"
+            loot_data += "\n"
+        
+        # Ajouter les informations sur le butin dans le fichier texte
+        for item in items:
+            loot_data += f"    item ID: {item['id']}\n"
+            loot_data += f"    Rarete: {item['rarete']}\n"
+            loot_data += f"    Nom: {item['item']['name']}\n"
+            loot_data += "\n"
+        
+        # Écrire les données dans un fichier texte
+        with open(f"./out/maison{i + 1}_{house_tier}.txt", "w", encoding="utf-8") as txt_file:
+            txt_file.write(loot_data)
 
-        with open("loot_data.json", "w") as json_file:
-            json.dump(loot_data, json_file, indent=4)
+def clean_out_directory():
+    directory = "./out"
+    for file in os.listdir(directory):
+        file_path = os.path.join(directory, file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(f"Erreur lors de la suppression du fichier {file_path}: {e}")
 
 def main():
+    clean_out_directory()
     draw_square()
     plt.imshow(background_image, extent=[0, 1, 0, 1], aspect='auto', alpha=0.7)  
     house_nbr = random.randint(30, 50)
@@ -140,7 +194,7 @@ def main():
     plt.axis('equal')
     plt.axis('off')
     plt.savefig('./out/map_no_circles.png', bbox_inches='tight')  # Enregistrer l'image sans les cercles
-    exit()
+
     generate_nested_circles(5)
 
 
